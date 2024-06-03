39e6067d5cdfc2fea0bb1263191bcd72f226e72f
iqbalzulsafari <iqbalzulsafari02@gmail.com>
2024-06-04 00:10:11 +0800

    .

--- app.py
+++ app.py
@@ -1,28 +1,51 @@
 from flask import Flask, render_template, request, jsonify
+from werkzeug.utils import secure_filename
+import os
+import chatbot_interview
+import resume_analyzer
 
 app = Flask(__name__)
 
+# Route for the main page
 @app.route('/')
-def index():
+def main_page():
     return render_template('main_page.html')
 
+# Route for the mockup interview page
+@app.route('/mockup_interview')
+def mockup_interview():
+    return render_template('ChatBot.html')
+
+# Route for handling chatbot interaction
+@app.route('/chatbot', methods=['POST'])
+def chatbot():
+    data = request.json
+    message = data['message']
+    # Process the message using the chatbot
+    response = chatbot_interview.process_message(message)
+    return jsonify({'response': response})
+
+# Route for the resume analyzer page
 @app.route('/resume_analyzer')
 def resume_analyzer():
     return render_template('resume1.html')
 
-@app.route('/resume_analyzer_result')
-def resume_analyzer_result():
-    return render_template('resume2.html')
+# Route for uploading resume
+@app.route('/upload_resume', methods=['POST'])
+def upload_resume():
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return jsonify({"filename": filename})
 
-@app.route('/chatbot', methods=['POST'])
-def chatbot():
-    # Handle chatbot requests here
-    # Example: process input, generate response, and return it
-    data = request.json
-    user_message = data['message']
-    # Process user_message and generate bot_response
-    bot_response = "This is a sample response from the chatbot."
-    return jsonify({'response': bot_response})
+# Route for analyzing resume and providing feedback
+@app.route('/analyze_resume', methods=['POST'])
+def analyze_resume():
+    filename = request.form['filename']
+    # Process the resume using the resume analyzer
+    feedback = resume_analyzer.process_resume(filename)
+    return jsonify({"feedback": feedback})
 
 if __name__ == '__main__':
+    app.config['UPLOAD_FOLDER'] = 'uploads'  # Specify the upload folder
     app.run(debug=True)
